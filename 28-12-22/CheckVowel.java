269a2b92915fdb0ba085a9be87fcb5b4a33ee31c
Nida Fatima <65632560+nidafatima01@users.noreply.github.com>
2023-02-08 02:43:52 +0530

    Add files via upload

---
+++ 28-12-22/CheckVowel.java
@@ -0,0 +1,16 @@
+package afternoon;
+
+public class CheckVowel {
+
+	public static void main(String[] args) {
+		
+		char ch = 'e';
+		
+		if((ch=='a')||(ch=='A')||(ch=='e')||(ch=='E')||(ch=='i')||(ch=='I')||(ch=='o')||(ch=='O')||(ch=='u')||(ch=='U'))
+			System.out.print(ch+ " is a vowel");
+		else
+			System.out.print(ch+ " is not a vowel");
+
+	}
+
+}
